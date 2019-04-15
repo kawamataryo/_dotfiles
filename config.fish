67c0a4c85ce6fca8dcc8f5a5cab803c53677c94b
ryokawamata <ba068082@gmail.com>
2019-04-15 20:09:12 +0900

    fzfの設定追加

--- config.fish
+++ config.fish
@@ -10,22 +10,33 @@ set -x fish_theme batman
 # fish_vi_key_bindings
 
 # ---------------------------------------------------------------------
-# ショートカット
+# envの設定
 # ---------------------------------------------------------------------
-# vim,vコマンドでnvimを開くようにする
-alias v "nvim"
-alias vi "nvim"
-alias vim "nvim"
+# anyenvの設定
+set -x PATH $HOME/.anyenv/bin $PATH
 
-# git alias
-alias ga  "git add"
-alias gaa  "git add ."
-alias gd  "git diff"
-alias gb  "git branch"
-alias gco "git checkout"
-alias gc "git commit"
-alias gs "git status"
-alias wp "php -d memory_limit=-1 /usr/local/bin/wp"
+# rbenv
+set -x RBENV_ROOT "$HOME/.anyenv/envs/rbenv"
+set -x PATH $PATH "$RBENV_ROOT/bin"
+status --is-interactive; and source (rbenv init -|psub)
+
+# nodenv
+set -x NODENV_ROOT $HOME/.anyenv/envs/nodenv
+set -x PATH $HOME/.anyenv/envs/nodenv/bin $PATH
+set -gx PATH $NODENV_ROOT/shims $PATH
+
+# pyenv
+set -x PYENV_ROOT "$HOME/.anyenv/envs/pyenv"
+set -x PATH $PATH "$PYENV_ROOT/bin"
+status --is-interactive; and source (pyenv init -|psub)
+
+# openssl
+set -x LIBRARY_PATH $LIBRARY_PATH "/usr/local/opt/openssl/lib/"
+
+# ---------------------------------------------------------------------
+# plugin設定
+# ---------------------------------------------------------------------
+set -U FZF_LEGACY_KEYBINDINGS 0
 
 # ---------------------------------------------------------------------
 # PATHの追加
@@ -42,112 +53,49 @@ set -x PATH $HOME/bin $PATH
 # opensslの設定
 set -x PATH /usr/local/opt/openssl/bin $PATH
 
-# compsoerの設定
-set -x PATH Unknown command /Users/kawamataryou/.composer/vendor/bin $PATH
-
-# ---------------------------------------------------------------------
-# envの設定
-# ---------------------------------------------------------------------
-# anyenvの設定
-set -x PATH $HOME/.anyenv/bin $PATH
-# rbenv
-set -x RBENV_ROOT "$HOME/.anyenv/envs/rbenv"
-set -x PATH $PATH "$RBENV_ROOT/bin"
-status --is-interactive; and source (rbenv init -|psub)
-
-# ndenv
-set -x NDENV_ROOT "$HOME/.anyenv/envs/ndenv"
-set -x PATH $PATH "$NDENV_ROOT/bin"
-set -gx PATH "$NDENV_ROOT/shims" $PATH
-set -gx NDENV_SHELL fish
-command ndenv rehash 2>/dev/null
-function ndenv
-  set command $argv[1]
-  set -e argv[1]
-
-  switch "$command"
-  case rehash shell
-    eval (ndenv sh-"$command" $argv|psub)
-  case '*'
-    command ndenv "$command" $argv
-  end
-end
-
-# pyenv
-set -x PYENV_ROOT "$HOME/.anyenv/envs/pyenv"
-set -x PATH $PATH "$PYENV_ROOT/bin"
-status --is-interactive; and source (pyenv init -|psub)
-
-# ---------------------------------------------------------------------
-# sshの設定
-# ---------------------------------------------------------------------
-
-# ssh-addの設定
-function ssh_makoto
-    ssh-add -K ~/.ssh/makotoxserver/id_rsa
-end
-
-function ssh_acuweb
-    ssh-add -K ~/.ssh/conoha_acuweb/id_rsa
-end
-
-function ssh_oasis
-    ssh-add -K ~/.ssh/oasisserver/id_rsa
-end
-
-function ssh_conoha
-    ssh-add -K ~/.ssh/conoha_patient_db/conoha_patient_db
-end
-
-function ssh_oasis
-    ssh-add -K ~/.ssh/oasisserver/id_rsa
-end
-
-function ssh_hattori
-    ssh-add -K ~/.ssh/hattorixserver/id_rsa
-end
-
-function ssh_conoha
-    ssh-add -K ~/.ssh/conoha_patient_db/id_rsa
-end
-
-function ssh_git
-    ssh-add -K ~/.ssh/id_git_rsa
-end
+# qt@5.5の設定
+set -x PATH /usr/local/opt/qt@5.5/bin $PATH
 
 # ---------------------------------------------------------------------
 # 便利alias
 # ---------------------------------------------------------------------
+# vim,vコマンドでnvimを開くようにする
+alias v "nvim"
+alias vi "nvim"
+alias vim "nvim"
 
-function gnow
-    git now
+# git alias
+alias ga  "git add"
+alias gaa  "git add ."
+alias gd  "git diff"
+alias gb  "git branch"
+alias gc "git checkout"
+alias gm "git commit"
+alias gs "git status"
+
+function ga
+  git add $arg
 end
 
 function gaa
-    git add .
+  git add .
 end
 
-function gc
-    git commit $argv
+function gm
+  git commit $arg
 end
 
-function gcm
-    git commit -m $argv
+# rmtrash
+function rmt
+  rmtrash $arg
 end
 
-function gs
-    git status $argv
+# fzfの設定
+alias gcf "git branch | fzf | xargs git checkout"
+
+# cran
+function cr
+  crane run
 end
 
-function gpush
-    git push $argv
-end
-
-function gpull
-    git pull $argv
-end
-
-function rm
-    rmtrash $argv
-end
-
+# func
